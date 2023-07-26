1af1b12a2b284e2c08b12b94c56de6b588b2f66b
naniwin <njonathan990@gmail.com>
2023-07-25 21:38:48 -0500

    package reinstallm testing component

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,14 @@
 import './App.css';
-import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// import React from 'react';
+// import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import AddManga from '../components/AddManga/AddManga';
 // import HomePage from '../pages/HomePage/HomePage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Routes>
-          <Route exact path="/" component={AddManga} />
-          {/* Add more routes for different pages here */}
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+      <AddManga />
+    </div>
   );
 }
 
